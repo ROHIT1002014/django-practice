4ad75dc019b61ee4fbffe02a253d42a7bcefe341
Rkverma1996 <sweetsanjeevkumar96@gmail.com>
2020-10-05 00:56:07 +0530

    implement product list page

--- ecommerse/ecommerse/views.py
+++ ecommerse/ecommerse/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
+from productInfo.models import Item
 
 def homePage(request):
+  products = Item.objects.all()[:4]
+  context = {'product_list': products, 'title': 'Product list'}
   # return TemplateResponse(request, 'entry_list.html', {'entries': Entry.objects.all()})
-  return TemplateResponse(request, 'index.html')
+  return TemplateResponse(request, 'home.html', context)
